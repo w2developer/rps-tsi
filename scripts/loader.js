68b0dbc97da32eb8675aa1e82cf5c663d038cc26
wmateus99 <wandersonmateus07@gmail.com>
2026-05-18 17:16:07 -0300

    Transferindo o Script da Tela de Carregamento para Dentro do index

--- scripts/loader.js
+++
@@ -1,6 +0,0 @@
-window.addEventListener('load', function() {
-    const loaderBox = document.querySelector('.box-loader');
-    if (loaderBox) {
-        loaderBox.style.display = 'none';
-    }
-});
